99c51c2835406b220fee194bacf1adc031399757
gigigigi1 <asalasanzz567@gmail.com>
2026-05-24 00:11:54 +0700

    remove all files

--- ios_src/ImGuiOverlay.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#import <UIKit/UIKit.h>
-#import <Metal/Metal.h>
-#import <MetalKit/MetalKit.h>
-
-@interface ImGuiOverlay : UIView
-+ (instancetype)sharedOverlay;
-- (void)setupImGui;
-@end
--- ios_src/imgui/imconfig.h
+++
@@ -1,7 +0,0 @@
-// imconfig.h
-#include <math.h>
-
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
